1c0a80106805fdba131d9a5768e7d58eb750cfdf
sameershaikh.x <gitshaikh@gmail.com>
2024-08-26 15:54:11 +0545

    ..

--- params.py
+++ params.py
@@ -1,5 +1,6 @@
 ## the {amount} in the funciton below is a parameter
 def getmilk(amount):
-    print(amount)
-    
+    vat = amount + 13/100 *amount
+    print(f'the original amount is {amount}')
+    print(f'the amount with vat is {vat}')
 getmilk(1200)
